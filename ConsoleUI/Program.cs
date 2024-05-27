2c26447f24b79e1e043157caa73b34ceb5fc033d
Abdullah FINDIK <Abdullah FINDIK@DESKTOP-3S58CUR>
2024-05-27 15:39:44 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+        foreach (Product product in productManager.GetAll())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
+}
